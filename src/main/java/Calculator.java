1aa9515e8bcea5c36ce5d0bea025a71d828c3bd5
Kirill Botalov <cccombo.breaker.yo@gmail.com>
2021-07-02 11:39:15 +0300

    Домашнее задание 7 - Калькулятор. Выполнил Боталов

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class Calculator {
+    private static String result;
+
+    public static String getResult() {
+        return result;
+    }
+
+    public static int sum(int num1, int num2) {
+        return num1 +  num2;
+    }
+
+    public static int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static int subtraction(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static int division(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    public static void calculation(String num1, String num2, String operation) {
+        int firstNumber = 0;
+        int secondNumber = 0;
+        try {
+            firstNumber = Integer.parseInt(num1);
+            secondNumber = Integer.parseInt(num2);
+            int result = 0;
+        } catch (NumberFormatException e) {
+            result = "Неверное значение";
+            return;
+        }
+        switch (operation) {
+            case "+" :
+                result = sum(firstNumber, secondNumber) + "";
+                break;
+            case "-" :
+                result = subtraction(firstNumber, secondNumber) + "";
+                break;
+            case  "*" :
+                result = multiply(firstNumber, secondNumber) + "";
+                break;
+            case "/" :
+                if (secondNumber==0) {
+                    result = "Нельзя делить на ноль";
+                    return;
+                }
+                 result = division(firstNumber, secondNumber) + "";
+                break;
+            default :
+                 result = "Такая операция не предусмотрена";
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+//        int number1 = 0;
+//        int number2 = 0;
+        System.out.println("Введите операцию");
+        String operation = scan.nextLine();
+        System.out.println("Введите первое число");
+        String number1 = scan.nextLine();
+        System.out.println("Введите второе число");
+        String number2 = scan.nextLine();
+
+        try {
+            calculation(number1, number2, operation);
+        } catch (NullPointerException e) {
+            System.out.println("Введены некорректные данные");
+        }
+
+    }
+
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,43 @@
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+
+public class Test {
+    @DataProvider
+    public Object [][] positiveTestProvider() {
+        return new Object[][]{
+                {"4", "2", "2", "+"},
+                {"-5", "5", "-10", "+"},
+                {"15", "-3", "-5", "*"},
+                {"-1", "-2", "-1", "-"},
+                {"-6", "2", "-3", "*"},
+                {"0", "0", "33", "/"}
+
+        };
+    }
+
+    @org.testng.annotations.Test(dataProvider = "positiveTestProvider")
+    public void positiveTest(String expected, String number1, String number2, String operation) {
+        Calculator.calculation(number1,number2,operation);
+        Assert.assertEquals(expected,Calculator.getResult());
+    }
+
+    @DataProvider
+    public Object[][] negativeTestProvider(){
+        return new Object[][]{
+                {"Нельзя делить на ноль", "3", "0", "/"},
+                {"Такая операция не предусмотрена", "23", "0", "="},
+                {"Неверное значение", "o", "3", "+"}
+        };
+    }
+
+    @org.testng.annotations.Test(dataProvider = "negativeTestProvider")
+    public void negativeTest(String expected, String number1, String number2, String operation) {
+        Calculator.calculation(number1,number2,operation);
+        Assert.assertEquals(expected,Calculator.getResult());
+    }
+
+
+
+
+
+}
